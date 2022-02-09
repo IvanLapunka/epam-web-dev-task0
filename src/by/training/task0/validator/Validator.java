b198696fd90f6c7e62a25085e1f437ee3f9a81fa
Ivan Lapunka <ivan_lapunka@gmail.com>
2022-02-10 01:56:56 +0300

    Make Validator singleton

---
+++ src/by/training/task0/validator/Validator.java
@@ -0,0 +1,5 @@
+package by.training.task0.validator;
+
+public interface Validator {
+    boolean isInteger(String value);
+}
---
+++ src/by/training/task0/validator/ValidatorImpl.java
@@ -0,0 +1,19 @@
+package by.training.task0.validator;
+
+import java.math.BigInteger;
+import java.util.regex.Pattern;
+
+public enum ValidatorImpl implements Validator {
+    INSTANCE;
+    private static final Pattern pattern = Pattern.compile("-?\\d+");
+
+    @Override public boolean isInteger(String value) {
+        if (!pattern.matcher(value).matches()) {
+            return false;
+        }
+
+        BigInteger test = new BigInteger(value);
+        int result = test.intValue();
+        return test.toString().equals(result + "");
+    }
+}
